67a95a2d12d2f56e27eb2ef4c07b58d1bf9a99d2
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-07-23 17:21:49 +0300

    adding jquery toggling effect for what we do icons

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+    $(".click1").click(function(){
+        $(".togglepara1").toggle(3000);
+    });
+    $(".click2").click(function(){
+        $(".togglepara2").toggle(3000);
+    });
+    $(".click3").click(function(){
+        $(".togglepara3").toggle(3000);
+    });
+});
