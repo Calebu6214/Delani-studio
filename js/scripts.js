801cd76a1de6cc62dd8012136cb6f241f8cb0410
Calebu6214 <caleb.kipngetich@student.moringaschool.com>
2021-07-24 04:38:42 +0300

    adding jquery hover effect on portfolio pics and styling

--- js/scripts.js
+++ js/scripts.js
@@ -8,7 +8,54 @@ $(document).ready(function(event){
     $(".click3").click(function(){
         $(".togglepara3").toggle(3000);
     });
+    $("#img1").hover(function(){
+        $("#hover1").show();
+    });
+    $("#img1").mouseout(function(){
+        $("#hover1").hide();
+    });
+    $("#img2").hover(function(){
+        $("#hover2").show();
+    });
+    $("#img2").mouseout(function(){
+        $("#hover2").hide();
+    });
+    $("#img3").hover(function(){
+        $("#hover3").show();
+    });
+    $("#img3").mouseout(function(){
+        $("#hover3").hide();
+    });
+    $("#img4").hover(function(){
+        $("#hover4").show();
+    });
+    $("#img4").mouseout(function(){
+        $("#hover4").hide();
+    });
 
-    
+    $("#img5").hover(function(){
+        $("#hover5").show();
+    });
+    $("#img5").mouseout(function(){
+        $("#hover5").hide();
+    });
+    $("#img6").hover(function(){
+        $("#hover6").show();
+    });
+    $("#img6").mouseout(function(){
+        $("#hover6").hide();
+    });
+    $("#img7").hover(function(){
+        $("#hover7").show();
+    });
+    $("#img7").mouseout(function(){
+        $("#hover7").hide();
+    });
+    $("#img8").hover(function(){
+        $("#hover8").show();
+    });
+    $("#img8").mouseout(function(){
+        $("#hover8").hide();
+    });
     event.preventDefault();
 });
